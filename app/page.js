0718379f60c24bd8cd39be46546c238886a4432d
Alexander Talamonti <a_talamonti@hotmail.com>
2024-11-12 10:57:44 -0500

    added loading feedback

--- app/page.js
+++ app/page.js
@@ -6,13 +6,16 @@ import NewsList from "@/components/NewsList";
 
 export default function Home() {
   const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleLocationSuccess = async (lat, lon) => {
     // fetch first gets new news and adds to database based on location
-    const res = await fetch(`http://localhost:8000/api/news/fetch`);
-    const {message} = await res.json();
-    console.log(message);
+    console.log("Fecthing any new news in the area");
+    setLoading(true);
+    // adds the new news to the database
+    await fetch(`http://localhost:8000/api/news/fetch`);
     setLocation({ lat, lon });
+    setLoading(false);
   };
 
   return (
@@ -24,6 +27,7 @@ export default function Home() {
           <>
             <p className="mb-4 text-lg">Click below to find happy news near you! Please wait a few seconds after answering the prompt :)</p>
             <LocationRequest onLocationSuccess={handleLocationSuccess} />
+            {loading && <p>Fetching news in the local area!</p>}
           </>
         ) : (
           <>
